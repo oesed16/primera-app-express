3beb1ee011cfa4678d59555a7853a613213f4446
Angela Castagno <oesed16@gmail.com>
2018-12-11 23:42:25 -0500

    Reto salúdame 2 Express

--- app.js
+++ app.js
@@ -9,4 +9,12 @@ app.get('/', (req, res) => {
     res.send(`<h1>Hola ${nombre}!</h1>`);
 });
 
+app.get('/makers/:nombre', (req, res) => { 
+    var nombre = req.params.nombre;
+    if (!nombre){
+        nombre = "desconocido";
+    }
+    res.send(`<h1>Hola ${nombre.charAt(0).toUpperCase() + nombre.slice(1)}!</h1>`);
+});
+
 app.listen(3000, () => console.log('Listening on port 3000!'));
