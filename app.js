e998b08c76b5f378960411fe6ea2f407f9631062
Angela Castagno <oesed16@gmail.com>
2018-12-17 23:02:43 -0500

    App Pares e Impares

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => { 
-    var nombre = req.query.nombre;
-    if (!nombre){
-        nombre = "desconocido";
+// app.get('/', (req, res) => { 
+//     var nombre = req.query.nombre;
+//     if (!nombre){
+//         nombre = "desconocido";
+//     }
+//     res.send(`<h1>Hola ${nombre}!</h1>`);
+// });
+
+const NUMBER_LINES = 50;
+
+app.get('/', (req, res) => {
+    var lines = "";
+    for (n = 0; n < NUMBER_LINES; n++) {
+        if ((n + 1) % 2 === 0) {
+            lines = lines + `<p>${n + 1} Soy Par!</p>`;
+        } else {
+            lines = lines + `<p>${n + 1} Soy Impar!</p>`;
+        }
     }
-    res.send(`<h1>Hola ${nombre}!</h1>`);
+    res.send(lines);
 });
 
-app.get('/makers/:nombre', (req, res) => { 
+app.get('/makers/:nombre', (req, res) => {
     var nombre = req.params.nombre;
-    if (!nombre){
+    if (!nombre) {
         nombre = "desconocido";
     }
     res.send(`<h1>Hola ${nombre.charAt(0).toUpperCase() + nombre.slice(1)}!</h1>`);
